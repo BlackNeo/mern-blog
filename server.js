0a491c856da41fa426ad205615ffaa3a946c1f78
BlackNeo <general.zsd@gmail.com>
2020-09-24 00:53:51 +0200

    DotEnv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-require('dotenv').config({ path: 'ENV_FILENAME' });
+require('dotenv').config();
 
 const app = express();
 const port = process.env.port || 8080;
