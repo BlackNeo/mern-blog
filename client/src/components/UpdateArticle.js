9503c7b8843cd59d3c55d27794c2a95f442d5181
BlackNeo <general.zsd@gmail.com>
2020-09-24 12:44:33 +0200

    127.0.0.1

--- client/src/components/UpdateArticle.js
+++ client/src/components/UpdateArticle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-const URI = "http://mern-stack-blog-geni.herokuapp.com:5000/articles/"
+const URI = "http://127.0.0.1:5000/articles/"
 
 class UpdateArticle extends React.Component {
     constructor(props){
